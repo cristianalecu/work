9ad8c6919b720f40c6700fae4e84a20b59389301
Cristian Alecu <cristian.alecu@gmail.com>
2017-10-17 18:29:03 +0300

    1

--- Ionic2/vsObjectives/vsObjectives/src/app/app.module.ts
+++ Ionic2/vsObjectives/vsObjectives/src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { Page1 } from '../pages/page1/page1';
-import { Page2 } from '../pages/page2/page2';
+
+import { ObjectivesPage } from '../pages/objectives/objectives';
+import { ObjectivePage } from '../pages/objective/objective';
+import { CategoryPage } from '../pages/category/category';
+import { CategoriesPage } from '../pages/categories/categories';
+import { AuthorsPage } from '../pages/authors/authors';
+import { AuthorPage } from '../pages/author/author';
+import { BooksPage } from '../pages/books/books';
+import { BookPage } from '../pages/book/book';
+import { TasksPage } from '../pages/tasks/tasks';
+import { TaskPage } from '../pages/task/task';
+import { SettingsPage } from '../pages/settings/settings';
 
 @NgModule({
   declarations: [
     MyApp,
-    Page1,
-    Page2
+    ObjectivesPage,
+    ObjectivePage,
+    CategoryPage,
+    CategoriesPage,
+    AuthorsPage,
+    AuthorPage,
+    BooksPage,
+    BookPage,
+    TasksPage,
+    TaskPage,
+    SettingsPage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -16,8 +35,17 @@ import { Page2 } from '../pages/page2/page2';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    Page1,
-    Page2
+    ObjectivesPage,
+    ObjectivePage,
+    CategoryPage,
+    CategoriesPage,
+    AuthorsPage,
+    AuthorPage,
+    BooksPage,
+    BookPage,
+    TasksPage,
+    TaskPage,
+    SettingsPage
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
