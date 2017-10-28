01300ae06d4be3b40f299462d5462a25cb238f5b
Cristian Alecu <cristian.alecu@gmail.com>
2017-10-28 13:27:10 +0300

    1

--- Ionic2/vsObjectives/vsObjectives/src/classes/storage.ts
+++ Ionic2/vsObjectives/vsObjectives/src/classes/storage.ts
@@ -1,18 +1,22 @@
 export enum SyncEvery { Never, Hour, Day, Week }  
 export enum ObjLevel { Supreem, Major, Current }  
 export enum RepeatType { None, Dayly, Weekly, Monthly, Yearly }    
-export enum tastType { Once, Numeric, Percentage }
+export enum valueType { Once, Numeric, Percentage }
 export enum ReminderType { None, OnTyme, HourBefore, DayBefore }
-export enum AlarmType { None, OnTyme, HourBefore, DayBefore }
+export enum AlarmType { None, Notify, Vibrate, Sound }
 
 export type SyncParams = { syncType: SyncEvery, last_datetime_sync: string, track_history: boolean, email?: string };
-export type Objective = { title: string, level: ObjLevel, priority: number, until: Date, picture: File, why: string, to_be: string; to_know: string; to_do: string };
+export type Objective = { title: string, level: ObjLevel, priority: number, until: Date, picture: string, why: string, to_be: string; to_know: string; to_do: string };
 export type Category = { title: string, about: string, picture: File };
 export type Task = {
   title: string, done: number, for_objective: Objective, for_categ: Category, start_date: Date, end_date: Date, priority: number, importance: number,
-  time: Date, repeat: RepeatType, alarm: AlarmType, picture: File, why: string, to_be: string; to_know: string; to_do: string
+  time: Date, repeat: RepeatType, alarm: AlarmType, target: valueType, targetValue: number, remind: ReminderType, every: number, fromDate: Date, duration: number,
+  notes: string, picture: string, 
 };
-
+export type Author = { title: string, about: string, picture: File }; 
+export type Book = { title: string, author: Author, category: Category, until: Date, pages: number, page: number, link: string, files: Array<File>, photos: Array<File> };
+  
+  
 export class Storage {
   version: number;
 
@@ -24,6 +28,6 @@ export class Storage {
 
   Save()
   {
-
+    //
   }
 }
