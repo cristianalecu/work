d7685224a9346f31281bbc839e56607fc38d7346
Cristian Alecu <cristian.alecu@gmail.com>
2018-01-30 01:17:15 +0200

    all

---
+++ push_all.bat
@@ -0,0 +1,33 @@
+call git config --global user.email "cristian.alecu@gmail.com"
+call git config --global user.name "Cristian Alecu"
+
+cd GitWork 
+call git push
+cd ..
+
+
+cd DeapLearn
+call git push
+cd ..
+
+
+cd Django
+call git push
+cd ..
+
+
+cd Financiar
+call git push
+cd ..
+
+
+cd Bets 
+call git push
+cd ..
+
+cd FinAng
+call git push
+cd ..
+
+
+:END_INIT
